ab99c184398e9e53c59a2db373681c8004d9c111
m0uwastaken <96599762+m0uwastaken@users.noreply.github.com>
2025-11-28 17:28:35 +0100

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,12 @@
+﻿export const metadata = {
+  title: 'EAS Calculator',
+  description: 'EAS Probability Calculator',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ page.js
@@ -0,0 +1,8 @@
+﻿export default function Home() {
+  return (
+    <div>
+      <h1>EAS Probability Calculator</h1>
+      <p>Welcome to your Next.js app!</p>
+    </div>
+  )
+}
